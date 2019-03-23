f81746825671c38688f281ad1dd0eb733c1a6963
dgarkavtseva <dgarkavtseva@umass.edu>
2019-03-23 17:12:58 -0400

    Updated third view to print a menu

--- src/App03.jsx
+++ src/App03.jsx
@@ -1,24 +1,84 @@
-// This is a place holder for the initial application state.
-const state = [
+const contentNode = document.getElementById('contents');
 
-];
+const issues = [
+  {
+  id: 1, status: 'Open', owner: 'Ravan',
+  created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+  title: 'Error in console when clicking Add',
+  },
+  {
+  id: 2, status: 'Assigned', owner: 'Eddie',
+  created: new Date('2016-08-16'), effort: 14,
+  completionDate: new Date('2016-08-30'),
+  title: 'Missing bottom border on panel',
+  },
+  ];
 
-// This grabs the DOM element to be used to mount React components.
-var contentNode = document.getElementById("contents");
-
-class MyComponent extends React.Component {
-  constructor() {
-    super();
-  }
-
+class Menu extends React.Component {
   render() {
     return (
-      <div>
-        <h1>My View 03</h1>
-      </div>
-    );
+      <div>These are the available options for grab and go today!</div>
+    ); 
   }
 }
 
-// This renders the JSX component inside the content node:
-ReactDOM.render(<MyComponent />, contentNode);
+class IssueTable extends React.Component {
+  render() {
+  const issueRows = this.props.issues.map(issue => <IssueRow
+  key={issue.id} issue={issue} />)
+  
+  return (
+  <table className="bordered-table">
+  <thead>
+  <tr>
+  <th>Id</th>
+  <th>Status</th>
+  <th>Owner</th>
+  <th>Created</th>
+  <th>Effort</th>
+  <th>Completion Date</th>
+  <th>Title</th>
+  </tr>
+  </thead>
+  <tbody>{issueRows}</tbody>
+  </table>
+  )
+  }
+  }
+
+
+class ItemsList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Menu</h1>
+        <Menu />
+        <hr />
+        <IssueTable issues={issues}/>
+        
+      </div>
+    ); 
+  }
+}
+
+
+
+
+class IssueRow extends React.Component {
+render() {
+const issue = this.props.issue;
+return (
+<tr>
+<td>{issue.id}</td>
+<td>{issue.status}</td>
+<td>{issue.owner}</td>
+<td>{issue.created.toDateString()}</td>
+<td>{issue.effort}</td>
+<td>{issue.completionDate ? issue.completionDate.toDateString() : ''}</td>
+<td>{issue.title}</td>
+</tr>
+)
+}
+}
+
+ReactDOM.render(<ItemsList />, contentNode);
