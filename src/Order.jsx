47ea023981b42e3b1d502bc0c90f9d256c02726a
dgarkavtseva <dgarkavtseva@umass.edu>
2019-03-24 14:27:51 -0400

    Updated order page to add an item and started the order form

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,6 +1,7 @@
-const contentNode = document.getElementById('contents');
 
-const issues = [
+const contentNode = document.getElementById("contents");
+
+const foods = [
   {
     order: 1, food: 'Chicken Tenders', diningHall: 'Worcester',
   },
@@ -11,57 +12,14 @@ const issues = [
     order: 3, food: 'Orange Chicken', diningHall: 'Frank',
   },
   {
-    order: 4, food: 'Sushi', diningHall: 'Worcester',    
+    order: 4, food: 'Sushi', diningHall: 'Worcester',   
   },
-];
+ ];
 
-class Menu extends React.Component {
-  render() {
-    return (
-      <div>These are the available options for grab and go today!</div>
-    ); 
-  }
-}
-
-class IssueTable extends React.Component {
-  render() {
-    const itemRows = this.props.issues.map(issue => <ItemRow
-    key={issue.order} issue={issue} />)
-    
-    return (
-      <table className="bordered-table">
-        <thead>
-          <tr>
-            <th>Option Number</th>
-            <th>Food</th>
-            <th>Dining Hall</th>
-          </tr>
-        </thead>
-        <tbody>{itemRows}</tbody>
-      </table>
-    )
-  }
-}
-
-
-class ItemsList extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Menu</h1>
-        <Menu />
-        <hr />
-        <IssueTable issues={issues}/>
-        
-      </div>
-    ); 
-  }
-}
-
-
-
-
-class ItemRow extends React.Component {
+ const orders = [
+ ];
+ 
+class IssueRow extends React.Component {
   render() {
     const issue = this.props.issue;
     return (
@@ -69,9 +27,82 @@ class ItemRow extends React.Component {
         <td>{issue.order}</td>
         <td>{issue.food}</td>
         <td>{issue.diningHall}</td>
+        
+       
       </tr>
-    )
+    );
   }
 }
 
-ReactDOM.render(<ItemsList />, contentNode);
+class ItemTable extends React.Component {
+  render() {
+    const issueRows = this.props.foods.map(issue => (
+      <IssueRow key={issue.id} issue={issue} />
+    ));
+    return (
+      <table className="bordered-table">
+        <thead>
+          <tr>
+            <th>Order Number</th>
+            <th>Food</th>
+            <th>Dining Hall</th>
+          </tr>
+        </thead>
+        <tbody>{issueRows}</tbody>
+      </table>
+    );
+  }
+}
+
+class Order extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <h1>Place an Order!</h1>
+        <h2>Fill out the form below</h2>
+        
+        
+      </div>
+    );  }
+}
+
+class IssueList extends React.Component {
+  constructor() {
+    super();
+    this.state = { foods: foods, orders: orders };
+    setTimeout(this.createItem.bind(this), 2000);
+  }
+
+  createNew(newIssue) {
+    const newIssues = this.state.foods.slice();
+    newIssue.order = this.state.foods.length + 1;
+    newIssues.push(newIssue);
+    this.setState({ foods: newIssues });
+  }
+
+  createItem() {
+    this.createNew({
+      order: 5,
+      food: 'Pasta',
+      diningHall: 'Hamp'
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Menu</h1>
+        <h2>These are the available options for grab and go today!</h2>
+        
+        <ItemTable foods={this.state.foods} />
+        <hr />
+        <Order />
+        <hr />
+        <ItemTable foods={this.state.foods} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<IssueList />, contentNode);
--- static/Order.js
+++ static/Order.js
@@ -8,9 +8,9 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var contentNode = document.getElementById('contents');
+var contentNode = document.getElementById("contents");
 
-var issues = [{
+var foods = [{
   order: 1, food: 'Chicken Tenders', diningHall: 'Worcester'
 }, {
   order: 2, food: 'Mac and Cheese', diningHall: 'Berk'
@@ -20,124 +20,18 @@ var issues = [{
   order: 4, food: 'Sushi', diningHall: 'Worcester'
 }];
 
-var Menu = function (_React$Component) {
-  _inherits(Menu, _React$Component);
+var orders = [];
 
-  function Menu() {
-    _classCallCheck(this, Menu);
+var IssueRow = function (_React$Component) {
+  _inherits(IssueRow, _React$Component);
 
-    return _possibleConstructorReturn(this, (Menu.__proto__ || Object.getPrototypeOf(Menu)).apply(this, arguments));
+  function IssueRow() {
+    _classCallCheck(this, IssueRow);
+
+    return _possibleConstructorReturn(this, (IssueRow.__proto__ || Object.getPrototypeOf(IssueRow)).apply(this, arguments));
   }
 
-  _createClass(Menu, [{
-    key: 'render',
-    value: function render() {
-      return React.createElement(
-        'div',
-        null,
-        'These are the available options for grab and go today!'
-      );
-    }
-  }]);
-
-  return Menu;
-}(React.Component);
-
-var IssueTable = function (_React$Component2) {
-  _inherits(IssueTable, _React$Component2);
-
-  function IssueTable() {
-    _classCallCheck(this, IssueTable);
-
-    return _possibleConstructorReturn(this, (IssueTable.__proto__ || Object.getPrototypeOf(IssueTable)).apply(this, arguments));
-  }
-
-  _createClass(IssueTable, [{
-    key: 'render',
-    value: function render() {
-      var itemRows = this.props.issues.map(function (issue) {
-        return React.createElement(ItemRow, {
-          key: issue.order, issue: issue });
-      });
-
-      return React.createElement(
-        'table',
-        { className: 'bordered-table' },
-        React.createElement(
-          'thead',
-          null,
-          React.createElement(
-            'tr',
-            null,
-            React.createElement(
-              'th',
-              null,
-              'Option Number'
-            ),
-            React.createElement(
-              'th',
-              null,
-              'Food'
-            ),
-            React.createElement(
-              'th',
-              null,
-              'Dining Hall'
-            )
-          )
-        ),
-        React.createElement(
-          'tbody',
-          null,
-          itemRows
-        )
-      );
-    }
-  }]);
-
-  return IssueTable;
-}(React.Component);
-
-var ItemsList = function (_React$Component3) {
-  _inherits(ItemsList, _React$Component3);
-
-  function ItemsList() {
-    _classCallCheck(this, ItemsList);
-
-    return _possibleConstructorReturn(this, (ItemsList.__proto__ || Object.getPrototypeOf(ItemsList)).apply(this, arguments));
-  }
-
-  _createClass(ItemsList, [{
-    key: 'render',
-    value: function render() {
-      return React.createElement(
-        'div',
-        null,
-        React.createElement(
-          'h1',
-          null,
-          'Menu'
-        ),
-        React.createElement(Menu, null),
-        React.createElement('hr', null),
-        React.createElement(IssueTable, { issues: issues })
-      );
-    }
-  }]);
-
-  return ItemsList;
-}(React.Component);
-
-var ItemRow = function (_React$Component4) {
-  _inherits(ItemRow, _React$Component4);
-
-  function ItemRow() {
-    _classCallCheck(this, ItemRow);
-
-    return _possibleConstructorReturn(this, (ItemRow.__proto__ || Object.getPrototypeOf(ItemRow)).apply(this, arguments));
-  }
-
-  _createClass(ItemRow, [{
+  _createClass(IssueRow, [{
     key: 'render',
     value: function render() {
       var issue = this.props.issue;
@@ -163,7 +57,150 @@ var ItemRow = function (_React$Component4) {
     }
   }]);
 
-  return ItemRow;
+  return IssueRow;
 }(React.Component);
 
-ReactDOM.render(React.createElement(ItemsList, null), contentNode);
+var ItemTable = function (_React$Component2) {
+  _inherits(ItemTable, _React$Component2);
+
+  function ItemTable() {
+    _classCallCheck(this, ItemTable);
+
+    return _possibleConstructorReturn(this, (ItemTable.__proto__ || Object.getPrototypeOf(ItemTable)).apply(this, arguments));
+  }
+
+  _createClass(ItemTable, [{
+    key: 'render',
+    value: function render() {
+      var issueRows = this.props.foods.map(function (issue) {
+        return React.createElement(IssueRow, { key: issue.id, issue: issue });
+      });
+      return React.createElement(
+        'table',
+        { className: 'bordered-table' },
+        React.createElement(
+          'thead',
+          null,
+          React.createElement(
+            'tr',
+            null,
+            React.createElement(
+              'th',
+              null,
+              'Order Number'
+            ),
+            React.createElement(
+              'th',
+              null,
+              'Food'
+            ),
+            React.createElement(
+              'th',
+              null,
+              'Dining Hall'
+            )
+          )
+        ),
+        React.createElement(
+          'tbody',
+          null,
+          issueRows
+        )
+      );
+    }
+  }]);
+
+  return ItemTable;
+}(React.Component);
+
+var Order = function (_React$Component3) {
+  _inherits(Order, _React$Component3);
+
+  function Order() {
+    _classCallCheck(this, Order);
+
+    return _possibleConstructorReturn(this, (Order.__proto__ || Object.getPrototypeOf(Order)).apply(this, arguments));
+  }
+
+  _createClass(Order, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'h1',
+          null,
+          'Place an Order!'
+        ),
+        React.createElement(
+          'h2',
+          null,
+          'Fill out the form below'
+        )
+      );
+    }
+  }]);
+
+  return Order;
+}(React.Component);
+
+var IssueList = function (_React$Component4) {
+  _inherits(IssueList, _React$Component4);
+
+  function IssueList() {
+    _classCallCheck(this, IssueList);
+
+    var _this4 = _possibleConstructorReturn(this, (IssueList.__proto__ || Object.getPrototypeOf(IssueList)).call(this));
+
+    _this4.state = { foods: foods, orders: orders };
+    setTimeout(_this4.createItem.bind(_this4), 2000);
+    return _this4;
+  }
+
+  _createClass(IssueList, [{
+    key: 'createNew',
+    value: function createNew(newIssue) {
+      var newIssues = this.state.foods.slice();
+      newIssue.order = this.state.foods.length + 1;
+      newIssues.push(newIssue);
+      this.setState({ foods: newIssues });
+    }
+  }, {
+    key: 'createItem',
+    value: function createItem() {
+      this.createNew({
+        order: 5,
+        food: 'Pasta',
+        diningHall: 'Hamp'
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'h1',
+          null,
+          'Menu'
+        ),
+        React.createElement(
+          'h2',
+          null,
+          'These are the available options for grab and go today!'
+        ),
+        React.createElement(ItemTable, { foods: this.state.foods }),
+        React.createElement('hr', null),
+        React.createElement(Order, null),
+        React.createElement('hr', null),
+        React.createElement(ItemTable, { foods: this.state.foods })
+      );
+    }
+  }]);
+
+  return IssueList;
+}(React.Component);
+
+ReactDOM.render(React.createElement(IssueList, null), contentNode);
