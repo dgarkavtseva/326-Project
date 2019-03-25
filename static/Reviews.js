fa9eb1aadc518aee2f24bd4e0a0e0d2b38fb907e
Brett <bmk997@gmail.com>
2019-03-25 15:32:13 -0400

    Updated Reviews page with table and add button to add reviews

--- static/Reviews.js
+++ static/Reviews.js
@@ -8,22 +8,250 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-// This is a place holder for the initial application state.
-var state = [];
+var issues = [{
+  name: "Joe Shmoe",
+  created: new Date("2019-03-24"),
+  time: "4:00pm",
+  orderFrom: "Baby Berk 1",
+  orderItem: "Hampshire Burger",
+  driver: "Bob Bobert",
+  review: "Food arrived warm, but in a open container, so the burger was tossed around and had fallen apart before it arrived"
+}, {
+  name: "Bob Bobert",
+  created: new Date("2019-03-25"),
+  time: "1:00pm",
+  orderFrom: "Frank Grab & Go",
+  orderItem: "Orange Chicken",
+  driver: "Joe Shmoe",
+  review: "Food arrived warm, and within 15 minutes of placing my order"
+}];
 
-// This grabs the DOM element to be used to mount React components.
 var contentNode = document.getElementById("contents");
 
-var MyComponent = function (_React$Component) {
-  _inherits(MyComponent, _React$Component);
+var IssueFilter = function (_React$Component) {
+  _inherits(IssueFilter, _React$Component);
 
-  function MyComponent() {
-    _classCallCheck(this, MyComponent);
+  function IssueFilter() {
+    _classCallCheck(this, IssueFilter);
 
-    return _possibleConstructorReturn(this, (MyComponent.__proto__ || Object.getPrototypeOf(MyComponent)).call(this));
+    return _possibleConstructorReturn(this, (IssueFilter.__proto__ || Object.getPrototypeOf(IssueFilter)).apply(this, arguments));
   }
 
-  _createClass(MyComponent, [{
+  _createClass(IssueFilter, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        "This is a placeholder for the ReviewFilter."
+      );
+    }
+  }]);
+
+  return IssueFilter;
+}(React.Component);
+
+var IssueRow = function IssueRow(props) {
+  return React.createElement(
+    "tr",
+    null,
+    React.createElement(
+      "td",
+      null,
+      props.issue.name
+    ),
+    React.createElement(
+      "td",
+      null,
+      props.issue.created.toDateString()
+    ),
+    React.createElement(
+      "td",
+      null,
+      props.issue.time
+    ),
+    React.createElement(
+      "td",
+      null,
+      props.issue.orderFrom
+    ),
+    React.createElement(
+      "td",
+      null,
+      props.issue.orderItem
+    ),
+    React.createElement(
+      "td",
+      null,
+      props.issue.driver
+    ),
+    React.createElement(
+      "td",
+      null,
+      props.issue.review
+    )
+  );
+};
+
+function IssueTable(props) {
+  var issueRows = props.issues.map(function (issue) {
+    return React.createElement(IssueRow, { key: issue.id, issue: issue });
+  });
+  return React.createElement(
+    "table",
+    { className: "bordered-table" },
+    React.createElement(
+      "thead",
+      null,
+      React.createElement(
+        "tr",
+        null,
+        React.createElement(
+          "th",
+          null,
+          "Name"
+        ),
+        React.createElement(
+          "th",
+          null,
+          "Date"
+        ),
+        React.createElement(
+          "th",
+          null,
+          "Time"
+        ),
+        React.createElement(
+          "th",
+          null,
+          "Ordered From"
+        ),
+        React.createElement(
+          "th",
+          null,
+          "Order"
+        ),
+        React.createElement(
+          "th",
+          null,
+          "Driver"
+        ),
+        React.createElement(
+          "th",
+          null,
+          "Review"
+        )
+      )
+    ),
+    React.createElement(
+      "tbody",
+      null,
+      issueRows
+    )
+  );
+}
+
+var IssueAdd = function (_React$Component2) {
+  _inherits(IssueAdd, _React$Component2);
+
+  function IssueAdd() {
+    _classCallCheck(this, IssueAdd);
+
+    var _this2 = _possibleConstructorReturn(this, (IssueAdd.__proto__ || Object.getPrototypeOf(IssueAdd)).call(this));
+
+    _this2.handleSubmit = _this2.handleSubmit.bind(_this2);
+    return _this2;
+  }
+
+  _createClass(IssueAdd, [{
+    key: "handleSubmit",
+    value: function handleSubmit(e) {
+      e.preventDefault();
+      var form = document.forms.issueAdd;
+      this.props.createIssue({
+        name: form.name.value,
+        time: form.time.value,
+        orderFrom: form.orderFrom.value,
+        orderItem: form.orderItem.value,
+        driver: form.driver.value,
+        review: form.review.value,
+        created: new Date()
+      });
+      // Clear the form for the next input.
+      form.name.value = '';
+      form.time.value = '';
+      form.orderFrom.value = '';
+      form.orderItem.value = '';
+      form.driver.value = '';
+      form.review.value = '';
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "form",
+          { name: "issueAdd", onSubmit: this.handleSubmit },
+          React.createElement("input", { type: "text", name: "name", placeholder: "Name" }),
+          React.createElement("input", { type: "text", name: "time", placeholder: "Time" }),
+          React.createElement("input", { type: "text", name: "orderFrom", placeholder: "Ordered From" }),
+          React.createElement("input", { type: "text", name: "orderItem", placeholder: "Order" }),
+          React.createElement("input", { type: "text", name: "driver", placeholder: "Driver" }),
+          React.createElement("input", { type: "text", name: "review", placeholder: "Review" }),
+          React.createElement(
+            "button",
+            null,
+            "Add"
+          )
+        )
+      );
+    }
+  }]);
+
+  return IssueAdd;
+}(React.Component);
+
+var IssueList = function (_React$Component3) {
+  _inherits(IssueList, _React$Component3);
+
+  function IssueList() {
+    _classCallCheck(this, IssueList);
+
+    var _this3 = _possibleConstructorReturn(this, (IssueList.__proto__ || Object.getPrototypeOf(IssueList)).call(this));
+
+    _this3.state = { issues: [] };
+
+    _this3.createIssue = _this3.createIssue.bind(_this3);
+    return _this3;
+  }
+
+  _createClass(IssueList, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      this.loadData();
+    }
+  }, {
+    key: "loadData",
+    value: function loadData() {
+      var _this4 = this;
+
+      setTimeout(function () {
+        _this4.setState({
+          issues: issues
+        });
+      }, 500);
+    }
+  }, {
+    key: "createIssue",
+    value: function createIssue(newIssue) {
+      var newIssues = this.state.issues.slice();
+      newIssue.id = this.state.issues.length + 1;
+      newIssues.push(newIssue);
+      this.setState({ issues: newIssues });
+    }
+  }, {
     key: "render",
     value: function render() {
       return React.createElement(
@@ -33,15 +261,20 @@ var MyComponent = function (_React$Component) {
           "h1",
           null,
           "Reviews"
-        )
+        ),
+        React.createElement(IssueFilter, null),
+        React.createElement("hr", null),
+        React.createElement(IssueTable, { issues: this.state.issues }),
+        React.createElement("hr", null),
+        React.createElement(IssueAdd, { createIssue: this.createIssue })
       );
     }
   }]);
 
-  return MyComponent;
+  return IssueList;
 }(React.Component);
 
 // This renders the JSX component inside the content node:
 
 
-ReactDOM.render(React.createElement(MyComponent, null), contentNode);
+ReactDOM.render(React.createElement(IssueList, null), contentNode);
