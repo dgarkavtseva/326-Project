3f6f8412b3a8a417325c571ed9d770eb909e32d6
kyle4765 <kg4765@gmail.com>
2019-03-25 23:30:10 -0400

    Finished homepage skeleton
    Added a description and form (nothing in backend yet) for index.html. Created index.css. Updated index.jsx to include form functionality.

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,23 +1,39 @@
-// This is a place holder for the initial application state.
-const state = [
-
-];
 
 // This grabs the DOM element to be used to mount React components.
 var contentNode = document.getElementById("contents");
 
 class MyComponent extends React.Component {
-  constructor() {
-    super();
-  }
 
-  render() {
-    return (
-      <div>
-        <h1>Homepage</h1>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+        this.state = { submitted: false };
+        this.handleSubmit = this.submitForm.bind(this);
+    }
+
+    submitForm(event) {
+        event.preventDefault();
+        console.log("PEBIS");
+        this.setState({submitted: true});
+    }
+
+    render() {
+        return (
+                <form name="temp" onSubmit={this.handleSubmit} id = "emailForm" id="emailForm">
+                    <div className="forms">
+                        <h4>Sign up now!</h4>
+                        <input type="text" name="firstName" placeholder="First Name" value={this.state.text} />
+                        <br></br>
+                        <input type="text" name="lastName" placeholder="Last Name" value={this.state.text} />
+                        <br></br>
+                        <input type="text" name="email" placeholder="Email" value={this.state.text} />
+                        <br></br>
+                        <button>Submit</button>
+                        {this.state.submitted ? <p>Thanks!</p> : <div></div>}
+                    </div>
+                </form>
+        );
+    }
 }
 
 // This renders the JSX component inside the content node:
--- static/Index.js
+++ static/Index.js
@@ -2,39 +2,73 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-// This is a place holder for the initial application state.
-var state = [];
-
 // This grabs the DOM element to be used to mount React components.
 var contentNode = document.getElementById("contents");
 
 var MyComponent = function (_React$Component) {
-  _inherits(MyComponent, _React$Component);
+    _inherits(MyComponent, _React$Component);
 
-  function MyComponent() {
-    _classCallCheck(this, MyComponent);
+    function MyComponent() {
+        _classCallCheck(this, MyComponent);
 
-    return _possibleConstructorReturn(this, (MyComponent.__proto__ || Object.getPrototypeOf(MyComponent)).call(this));
-  }
+        var _this = _possibleConstructorReturn(this, (MyComponent.__proto__ || Object.getPrototypeOf(MyComponent)).call(this));
 
-  _createClass(MyComponent, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        null,
-        null
-      );
+        _this.state = { submitted: false };
+        _this.handleSubmit = _this.submitForm.bind(_this);
+        return _this;
     }
-  }]);
 
-  return MyComponent;
+    _createClass(MyComponent, [{
+        key: "submitForm",
+        value: function submitForm(event) {
+            event.preventDefault();
+            console.log("PEBIS");
+            this.setState({ submitted: true });
+        }
+    }, {
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "form",
+                _defineProperty({ name: "temp", onSubmit: this.handleSubmit, id: "emailForm" }, "id", "emailForm"),
+                React.createElement(
+                    "div",
+                    { className: "forms" },
+                    React.createElement(
+                        "h3",
+                        null,
+                        "Sign up now!"
+                    ),
+                    React.createElement("input", { type: "text", name: "firstName", placeholder: "First Name", value: this.state.text }),
+                    React.createElement("br", null),
+                    React.createElement("input", { type: "text", name: "lastName", placeholder: "Last Name", value: this.state.text }),
+                    React.createElement("br", null),
+                    React.createElement("input", { type: "text", name: "email", placeholder: "Email", value: this.state.text }),
+                    React.createElement("br", null),
+                    React.createElement(
+                        "button",
+                        null,
+                        "Submit"
+                    ),
+                    this.state.submitted ? React.createElement(
+                        "p",
+                        null,
+                        "Thanks!"
+                    ) : React.createElement("div", null)
+                )
+            );
+        }
+    }]);
+
+    return MyComponent;
 }(React.Component);
 
 // This renders the JSX component inside the content node:
