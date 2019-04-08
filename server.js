58280a01e21fd14bfba0fd9a59bf9f2576a0f594
mpearce25 <pearcematthew25@gmail.com>
2019-04-07 22:50:13 -0400

    connections to DB's

--- server.js
+++ server.js
@@ -11,3 +11,22 @@ const MongoClient = require('mongodb').MongoClient;
 app.listen(3000, function () {
     console.log('App started on port 3000');
 });
+//////////
+
+
+
+let orderDB;
+let reviewDB;
+let userSignUpDB;
+
+MongoClient.connect('mongodb://localhost', { useNewUrlParser: true }).then(connection => {
+    orderDB = connection.db('CS326-DataBase-Orders');
+    reviewDB = connection.db('CS326-DataBase-Reviews');
+    userSignUpDB = connection.db('CS326-Users');
+
+  app.listen(3000, () => {
+    console.log('App started on port 3000');
+  });
+}).catch(error => {
+  console.log('ERROR:', error);
+});
