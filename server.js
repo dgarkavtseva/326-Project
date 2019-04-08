0541e1d871de43711c3fb0a4e4e755321c8c35e7
Brett <bmk997@gmail.com>
2019-04-07 22:06:26 -0400

    Added JSON Middleware support

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(express.static('static'));
+app.use(bodyParser.json());
+
+const MongoClient = require('mongodb').MongoClient;
 
 app.listen(3000, function () {
     console.log('App started on port 3000');
