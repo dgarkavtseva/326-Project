2c75ba4ae8231e7cfd1ba710c0ca28dec33d5493
mpearce25 <pearcematthew25@gmail.com>
2019-04-10 01:18:47 -0400

    changing order number init

--- scripts/init.mongo.js
+++ scripts/init.mongo.js
@@ -66,6 +66,6 @@ deliveriesDB.deliveries.insert([
 
 placedOrdersDB.placedOrders.insert([
   {
-      orderNumber: '5444', status: "pending", deliveryAdress: 'JQA'
+      orderNumber: '3', status: "pending", deliveryAdress: 'JQA'
   },
 ]);
